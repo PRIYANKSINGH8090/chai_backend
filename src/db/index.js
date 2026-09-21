85891e87b568b5059477261ca0aa41cc12b9ae6d
priyanksingh10 <singhpriyank2006@gmail.com>
2026-09-21 22:11:39 +0530

    add mongodb connection and dotenv setup

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import { DB_NAME } from "../constants.js";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+
+    console.log(
+      `\n MongoDB connected !! ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("ERROR", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
